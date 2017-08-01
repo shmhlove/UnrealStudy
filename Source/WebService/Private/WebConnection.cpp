cdbe79f9c440acf0dee06f6d45c6073a7aa74ba1
이상호 <shmhlove@ncsoft.com>
2017-08-01 09:55:12 +0900

    DebugEditor 빌드로 실행되도록 ( 나머지 빌드에서는 컴파일 오류 발생 함 )

--- Source/WebService/Private/WebConnection.cpp
+++ Source/WebService/Private/WebConnection.cpp
@@ -1,6 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "../Pubilc/WebService.h"
+//#include "../Pubilc/WebService.h"
 #include "WebConnection.h"
 
 DEFINE_LOG_CATEGORY(WebConnection);
